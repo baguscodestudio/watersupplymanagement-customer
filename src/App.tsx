13f2a4fd39a5726a8779d8e4d9c96bd627d74e28
BagusCodeStudio <60974759+baguscodestudio@users.noreply.github.com>
2022-07-02 20:06:24 +0800

    Initial Commit.

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useState, createContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+const userObj = {
+  user: {
+    userId: "",
+    username: "",
+    password: "",
+    createdAt: "",
+    fullName: "",
+    gender: "M",
+    email: "",
+    phone: "",
+    type: "",
+    lastMaintenance: "",
+  },
+  setUser: (user: UserType) => {},
+};
+
+export const UserContext = createContext(userObj);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="h-screen flex flex-col">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        pauseOnFocusLoss={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer />
+      <Router>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Routes>
+            {/* <Route path="/" element={<Login />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route> */}
+          </Routes>
+        </UserContext.Provider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
